28721dacaea3836b706b98065995bd27846d2c2a
kphed <kpcrypto69@gmail.com>
2024-03-20 11:00:19 -0400

    Add constructor test

--- test/ElonRWA.t.sol
+++ test/ElonRWA.t.sol
@@ -4,4 +4,17 @@ pragma solidity ^0.8.20;
 import "forge-std/Test.sol";
 import {ElonRWA} from "src/ElonRWA.sol";
 
-contract ElonRWATest is Test {}
+contract ElonRWATest is Test {
+    string public constant NAME = "ElonRWA";
+    string public constant SYMBOL = "ElonRWA";
+    uint256 public constant TOTAL_SUPPLY = 100e9;
+
+    function testConstructor() external {
+        ElonRWA elonRWA = new ElonRWA();
+
+        assertEq(elonRWA.name(), NAME);
+        assertEq(elonRWA.symbol(), SYMBOL);
+        assertEq(elonRWA.totalSupply(), TOTAL_SUPPLY);
+        assertEq(elonRWA.balanceOf(address(this)), TOTAL_SUPPLY);
+    }
+}
